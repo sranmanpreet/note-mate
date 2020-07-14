6739b2d99d9dcd1ba4cc254a2b3785a2d6465d29
sranmanpreet <manna1661@gmail.com>
2020-07-14 09:07:14 +0530

    Search enabled

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -10,12 +10,47 @@ import { NoteService } from 'src/app/shared/note.service';
 export class NotesListComponent implements OnInit {
 
   notes: Note[];
+  filteredNotes: Note[] = new Array<Note>();
   searchText;
 
   constructor(private noteService: NoteService) { }
 
   ngOnInit(): void {
     this.notes= this.noteService.getAll();
+    this.filteredNotes = this.notes;
+  }
+
+  filter(query: string){
+    let allResults: Note[] = new Array<Note>();
+
+    query = query.toLowerCase().trim();
+    let terms: string[] = query.split(' ');
+
+    terms = this.removeDuplicates(terms);
+    let results:Note[] =[];
+    terms.forEach(term=>{
+      results = this.relevantNotes(term);
+      allResults = [...allResults, ...results];
+    });
+
+    this.filteredNotes = this.removeDuplicates(allResults);
+  }
+
+  removeDuplicates(arr: Array<any>): Array<any>{
+    let uniqueResults: Set<any> = new Set<any>();
+    arr.forEach(element => uniqueResults.add(element));
+    return Array.from(uniqueResults);
+  }
+
+  relevantNotes(query: string): Array<Note>{
+    query = query.toLowerCase().trim();
+    let relevantNotes = this.notes.filter(note => {
+      if((note.title && note.title.toLowerCase().includes(query)) || (note.body && note.body.toLowerCase().includes(query))){
+        return true;
+      }
+      return false;
+    });
+    return relevantNotes;
   }
 
 }
--- src/app/shared/note.service.ts
+++ src/app/shared/note.service.ts
@@ -11,6 +11,18 @@ export class NoteService {
     { title : "Buy Sandles", body: "Buy Sandles for Sartaj"},
     { title : "Complete NoteMate", body: "Complete NoteApp development by Thursday"},
     { title : "Buy Vegetables", body: "Buy Vegies"},
+    { title : "Buy Cookies", body: "Buy Tiger for Sartaj, OREO for Joe"},
+    { title : "Buy Sandles", body: "Buy Sandles for Sartaj"},
+    { title : "Complete NoteMate", body: "Complete NoteApp development by Thursday"},
+    { title : "Buy Vegetables", body: "Buy Vegies"},
+    { title : "Buy Cookies", body: "Buy Tiger for Sartaj, OREO for Joe"},
+    { title : "Buy Sandles", body: "Buy Sandles for Sartaj"},
+    { title : "Complete NoteMate", body: "Complete NoteApp development by Thursday"},
+    { title : "Buy Vegetables", body: "Buy Vegies"},
+    { title : "Buy Cookies", body: "Buy Tiger for Sartaj, OREO for Joe"},
+    { title : "Buy Sandles", body: "Buy Sandles for Sartaj"},
+    { title : "Complete NoteMate", body: "Complete NoteApp development by Thursday"},
+    { title : "Buy Vegetables", body: "Buy Vegies"}
   ];
 
   constructor() {
